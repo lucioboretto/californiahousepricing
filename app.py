2a594f0b5bf9c2968565605081dce75905aec110
Lucio Boretto <lucio.boretto@gmail.com>
2026-03-23 14:03:59 +0100

    This is the commit for the web app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pickle
-from flask import Flask,request,app,josonify,url_for,render_template
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -15,8 +16,10 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    new_data=[list(data.values())]
-    print(new_data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+if __name__=="__main__":    app.run(debug=True)
